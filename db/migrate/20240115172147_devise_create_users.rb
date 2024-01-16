d79b55851e7e8958261c87bcfd49cc83ec9cfb0d
lily4178993 <nellytelli@gmail.com>
2024-01-16 23:42:31 +0000

    db: add confirmable module to User model

--- db/migrate/20240115172147_devise_create_users.rb
+++ db/migrate/20240115172147_devise_create_users.rb
@@ -19,13 +19,19 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       ## Rememberable
       t.datetime :remember_created_at
 
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
+
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end
