860c82904e90ac8722ddb509f9a4a523fc27e088
lily4178993 <nellytelli@gmail.com>
2024-01-16 23:38:02 +0000

    chore: update routes.rb to use custom sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users, controllers: { sessions: 'users/sessions' }
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "splash#index"
-
   authenticated :user do
     root "groups#index", as: :authenticated_root
   end
+
+  root "splash#index"
 
   resources :users do
     resources :groups, only: [:index, :show, :create, :destroy] do
