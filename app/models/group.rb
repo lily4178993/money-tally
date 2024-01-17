c696cf9681fb082f7565bd4c3d68eb6dae4329f2
lily4178993 <nellytelli@gmail.com>
2024-01-17 16:33:54 +0000

    model(Group): Add recent_groups method

--- app/models/group.rb
+++ app/models/group.rb
@@ -9,4 +9,8 @@ class Group < ApplicationRecord
   def self.total_groups
     Group.all.count(:id)
   end
+
+  def self.recent_groups
+    Group.order(created_at: :desc)
+  end
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Group, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:icon) }
     it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:icon) }
     it { should validate_length_of(:name).is_at_least(3).is_at_most(50) }
   end
 
@@ -18,6 +18,15 @@ RSpec.describe Group, type: :model do
       it 'returns the total number of groups' do
         create_list(:group, 3)
         expect(Group.total_groups).to eq(3)
+      end
+    end
+
+    describe '.recent_groups' do
+      it 'returns the groups in descending order of creation' do
+        group1 = create(:group)
+        group2 = create(:group)
+        group3 = create(:group)
+        expect(Group.recent_groups).to eq([group3, group2, group1])
       end
     end
   end
