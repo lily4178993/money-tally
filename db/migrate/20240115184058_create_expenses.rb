a04d9d217a2caec52ab7d3322730a97af43ec6dc
lily4178993 <nellytelli@gmail.com>
2024-01-17 01:00:54 +0000

    db: update precision and scale for amount column in expenses table

--- db/migrate/20240115184058_create_expenses.rb
+++ db/migrate/20240115184058_create_expenses.rb
@@ -3,7 +3,7 @@ class CreateExpenses < ActiveRecord::Migration[7.1]
     create_table :expenses do |t|
       t.references :author, null: false, foreign_key: { to_table: :users }
       t.string :name
-      t.decimal :amount
+      t.decimal :amount, precision: 10, scale: 2, default: 0.0
 
       t.timestamps
     end
