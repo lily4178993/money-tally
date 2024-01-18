13915b29c54d796183810636cec669568617e6ac
lily4178993 <nellytelli@gmail.com>
2024-01-18 12:08:35 +0000

    refactor(ApplicationController): Update sign up params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
   before_action :configure_sign_up_params, if: :devise_controller?
-
-  rescue_from CanCan: :AccessDenied do |exception|
-    redirect_to main_app.root_url, alert: exception.message
-  end
 
   protected
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up) do |user|
-      user.permit(:name, :email, :password, :password_confirmation)
+      user.permit(:profile_photo, :name, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:sign_in) do |user|
       user.permit(:email, :password)
