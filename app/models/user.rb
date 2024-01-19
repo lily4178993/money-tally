3124f3c1b9c8d135f6c5c2f03472f6e85d1e0810
lily4178993 <nellytelli@gmail.com>
2024-01-19 15:05:58 +0000

    refactor(user model): update validations and add password change functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,39 @@ class User < ApplicationRecord
 
   validates :name, presence: true, allow_blank: false, length: { minimum: 3, maximum: 50 }
   validates :email, presence: true, allow_blank: false
-  validates :password, presence: true, allow_blank: false, length: { minimum: 6, maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6, maximum: 30 }, if: :password_required?
+  validates :password, confirmation: true, if: :password_confirmation
+  validates :current_password, presence: true, if: :change_password?
 
   before_save :set_default_profile_photo
+
+  attr_accessor :current_password, :change_password, :password_confirmation
+
+  def change_password?
+    change_password == '1'
+  end
+
+  def update_with_password(params, *options)
+    update(params, *options)
+  end
+
+  def update_without_password(params, *options)
+    params.delete(:password)
+    params.delete(:password_confirmation)
+    params.delete(:current_password)
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
 
   private
 
   def set_default_profile_photo
     self.profile_photo ||= 'images/default_profile_photo.svg'
+  end
+
+  def password_required?
+    !persisted? || !password.nil? || change_password? || !password_confirmation.nil?
   end
 end
