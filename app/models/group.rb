f368c7175f18e70077acd0d62ba9e2931a5af77c
lily4178993 <nellytelli@gmail.com>
2024-01-24 00:16:43 +0000

    refactor(Group model): Add before_destroy callback to destroy associated expenses

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,8 @@ class Group < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :expenses
 
+  before_destroy :destroy_expenses
+
   validates :name, presence: true, allow_blank: false, length: { minimum: 3, maximum: 50 }
   validates :user_id, presence: true, allow_blank: false
   validates :icon, presence: true, allow_blank: false
@@ -12,5 +14,13 @@ class Group < ApplicationRecord
 
   def self.recent_groups
     Group.order(created_at: :desc)
+  end
+
+  private
+
+  def destroy_expenses
+    expenses.each do |expense|
+      expense.destroy
+    end
   end
 end
