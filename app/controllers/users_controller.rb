909b5325a3ad1ae038b22bf1a49680e81de4c095
lily4178993 <nellytelli@gmail.com>
2024-01-24 21:06:25 +0000

    test(UsersController): Add Devise and Warden test helpers to rails_helper.rb and update UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def show
+  def profile
+    @user = current_user
+  end
+
+  def statistics
+    puts "Current user: #{current_user.inspect}"
     @user = current_user
     @total_groups = Group.total_groups(current_user)
-    @total_expenses = @user.expenses.total_expenses
+    @total_expenses = Expense.total_expenses(current_user)
 
     @total_expenses_by_group = @user.groups.includes(:expenses).to_h do |group|
       [group.name, Expense.total_expenses_by_group(group)]
@@ -22,39 +27,5 @@ class UsersController < ApplicationController
          Expense.total_expenses_by_group_and_month(group, date.beginning_of_month..date.end_of_month)]
       end]
     end
-  end
-
-  def edit
-    @user = current_user
-  end
-
-  def update
-    successfully_updated = if user_params[:change_password].present?
-                             current_user.update_with_password(user_params)
-                           else
-                             current_user.update_without_password(user_params)
-                           end
-
-    if successfully_updated
-      bypass_sign_in(current_user)
-      flash[:notice] = 'Your account has been updated!'
-      redirect_to user_path(current_user)
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    current_user.destroy
-    flash[:notice] = 'Your account has been deleted!'
-    redirect_to root_path
-  end
-
-  private
-
-  def user_params
-    permitted_params = %i[profile_photo name email current_password]
-    permitted_params += %i[change_password password password_confirmation] if params[:user][:change_password] == '1'
-    params.require(:user).permit(permitted_params)
   end
 end
