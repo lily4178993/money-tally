25172e271961c227a76b524c01a4c18eb07c0f73
lily4178993 <nellytelli@gmail.com>
2024-01-25 00:30:42 +0000

    test(ControllerSpecs): Fix controller specs and add missing parameters

--- spec/controllers/expenses_controller_spec.rb
+++ spec/controllers/expenses_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ExpensesController, type: :controller do
   include Devise::Test::ControllerHelpers
 
-  let(:user) { create(:user) }
+  let(:user) { create(:user, confirmed_at: Time.now) }
   let(:group) { create(:group, user:) }
   let(:expense) { create(:expense, author: user, groups: [group]) }
 
@@ -12,23 +12,23 @@ RSpec.describe ExpensesController, type: :controller do
   end
 
   describe 'GET #new' do
-    it 'redirects to the correct path' do
-      get :new, params: { user_id: user.id, group_id: group.id }
-      expect(response).to be_redirect
+    it 'returns http success' do
+      get :new, params: { group_id: group.id }
+      expect(response).to be_successful
     end
   end
 
   describe 'POST #create' do
-    it 'redirects to the correct path' do
-      post :create, params: { user_id: user.id, group_id: group.id }
-      expect(response).to be_redirect
+    it 'redirects to the group show page' do
+      post :create, params: { group_id: group.id, expense: { name: 'Test Expense', amount: 10 } }
+      expect(response).to redirect_to(group_path(group))
     end
   end
 
   describe 'DELETE #destroy' do
-    it 'redirects to the correct path' do
-      delete :destroy, params: { user_id: user.id, group_id: group.id, id: expense.id }
-      expect(response).to be_redirect
+    it 'redirects to the group show page' do
+      delete :destroy, params: { group_id: group.id, id: expense.id }
+      expect(response).to redirect_to(group_path(group))
     end
   end
 end
