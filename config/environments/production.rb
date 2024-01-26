8eb02113a49061dfef508ae42314cf0257c78d8b
lily4178993 <nellytelli@gmail.com>
2024-01-26 03:21:41 +0000

    feat(Mailer): Use environment variable for Action Mailer host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "money_tally_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: ENV['APP_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
